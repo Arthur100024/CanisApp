0321bbc950e7ce8052f9cc8a4b7f3a7ee640aeac
Arthur <justfrog44@gmail.com>
2022-10-01 19:50:27 +0300

    add SharedPrefs LOGGED

--- app/src/main/java/com/karapetyanarthur/canisapp/Activities/MainActivity.java
+++ app/src/main/java/com/karapetyanarthur/canisapp/Activities/MainActivity.java
@@ -3,23 +3,34 @@ package com.karapetyanarthur.canisapp.Activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import com.karapetyanarthur.canisapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private SharedPreferences SharedPrefs;
+    public static final String LOGGED = "IsUserLogged";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SharedPrefs = getSharedPreferences("prefs",MODE_PRIVATE);
 
         try {
             Thread.sleep(3000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        changeActivity(".EnterActivity");
+        if (SharedPrefs.getString(LOGGED, "0").equals("0")){
+            changeActivity(".EnterActivity");
+        } else if (SharedPrefs.getString(LOGGED, "0").equals("1")){
+            changeActivity(".NavigationActivity");
+        }
+
     }
 
     public void changeActivity(String name_of_activity){
--- app/src/main/java/com/karapetyanarthur/canisapp/Activities/MapSetLocActivity.java
+++ app/src/main/java/com/karapetyanarthur/canisapp/Activities/MapSetLocActivity.java
@@ -2,6 +2,13 @@ package com.karapetyanarthur.canisapp.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -22,6 +29,9 @@ public class MapSetLocActivity extends AppCompatActivity {
 
     Button apply_loc_btn;
 
+    private SharedPreferences SharedPrefs;
+    public static final String LOGGED = "IsUserLogged";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,23 +41,29 @@ public class MapSetLocActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_map_set_loc);
 
+        {
+            apply_loc_btn = findViewById(R.id.apply_loc_btn);
+            map_view = (MapView) findViewById(R.id.mapview);
+            SharedPrefs = getSharedPreferences("prefs",MODE_PRIVATE);
+        }
 
-        apply_loc_btn = findViewById(R.id.apply_loc_btn);
-
-        map_view = (MapView)findViewById(R.id.mapview);
-
+//ПЕРЕМЕЩЕНИЕ КАМЕРЫ
         map_view.getMap().move(
                 new CameraPosition(TARGET_LOCATION, 18.0f, 0.0f, 0.0f),
                 new Animation(Animation.Type.SMOOTH, 10f),
                 null);
 
-        map_view.getMap().getMapObjects().addPlacemark(TARGET_LOCATION, ImageProvider.fromResource(R.drawable.hide_password_icon));
+//ДОБАВЛЕНИЕ МАРКЕРА
+        map_view.getMap().getMapObjects().addPlacemark(TARGET_LOCATION, ImageProvider.fromBitmap(drawSimpleBitmap("Я")));
 
 
         apply_loc_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                SharedPreferences.Editor editorLogged = SharedPrefs.edit();
+                editorLogged.putString(LOGGED,"1");
+                editorLogged.apply();
+                changeActivity(".NavigationActivity");
             }
         });
     }
@@ -64,5 +80,30 @@ public class MapSetLocActivity extends AppCompatActivity {
         map_view.onStop();
         MapKitFactory.getInstance().onStop();
         super.onStop();
+    }
+
+//БИТМАП ДЛЯ МАРКЕРА
+    public Bitmap drawSimpleBitmap(String number) {
+        int picSize = 100;
+        Bitmap bitmap = Bitmap.createBitmap(picSize, picSize, Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        // отрисовка плейсмарка
+        Paint paint = new Paint();
+        paint.setColor(getResources().getColor(R.color.activity_background_clr));
+        paint.setStyle(Paint.Style.FILL);
+        canvas.drawCircle(picSize / 2, picSize / 2, picSize / 2, paint);
+        // отрисовка текста
+        paint.setColor(Color.WHITE);
+        paint.setAntiAlias(true);
+        paint.setTextSize(30);
+        paint.setTextAlign(Paint.Align.CENTER);
+        canvas.drawText(number, picSize / 2,
+                picSize / 2 - ((paint.descent() + paint.ascent()) / 2), paint);
+        return bitmap;
+    }
+
+    public void changeActivity(String name_of_activity){
+        Intent changeMyActivity = new Intent(name_of_activity);
+        startActivity(changeMyActivity);
     }
 }
