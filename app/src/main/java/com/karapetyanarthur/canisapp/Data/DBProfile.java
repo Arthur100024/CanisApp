d5bc5a4ab0f5c2936779a800ecb333b8d3cae3d8
Arthur100024 <justfrog44@gmail.com>
2022-10-11 23:36:25 +0300

    pr 1 task 2

---
+++ app/src/main/java/com/karapetyanarthur/canisapp/Data/DBProfile.java
@@ -0,0 +1,83 @@
+package com.karapetyanarthur.canisapp.Data;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.Ignore;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "profile")
+public class DBProfile {
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "profile_id")
+    private long id;
+    @ColumnInfo(name = "profile_email")
+    private String email;
+    @ColumnInfo(name = "profile_name")
+    private String name;
+    @ColumnInfo(name = "profile_surname")
+    private String surname;
+    @ColumnInfo(name = "profile_phone")
+    private String phone;
+    @ColumnInfo(name = "profile_age")
+    private String age;
+
+    @Ignore
+    public DBProfile(){}
+
+    public DBProfile(long id, String email, String name, String surname, String phone, String age){
+        this.id = id;
+        this.email = email;
+        this.name = name;
+        this.surname = surname;
+        this.phone = phone;
+        this.age = age;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+}
